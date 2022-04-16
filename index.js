e6ee7ca99583613f60c6bd5aba63266037019e39
naejbravo <naejbravo@github.com>
2022-04-16 19:02:38 +0200

    nose3

--- index.js
+++ index.js
@@ -21,18 +21,19 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.use(
-  cors({
-    origin: [
-      "https://agenda-fe.vercel.app/",
-      // "https://angular-continent-countries.vercel.app", "http://localhost:4200"
-    ],
-    //credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "https://agenda-fe.vercel.app/",
+//       // "https://angular-continent-countries.vercel.app", "http://localhost:4200"
+//     ],
+//     //credentials: true,
+//   })
+// );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
